c912682783d8b031846456bbaab6703599d5b9d4
Abbas Dohadwala <abbas5dohadwala@gmal.com>
2021-05-10 12:22:27 -0700

    made some changes, added some comments

--- 3dMazeGameCapstone/src/Block.java
+++ 3dMazeGameCapstone/src/Block.java
@@ -1,4 +1,24 @@
+import processing.core.PApplet;
 
 public class Block {
-
+	
+	public final int x, y, z;
+	public Maze maze;
+	public char t; // type of block
+	
+	public Block (int x, int y, int z, char t) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.t = t;
+	}
+	
+	public void add (Maze maze) {
+		this.maze = maze;
+	}
+	
+	public void draw (PApplet g) {
+		
+	}
+	
 }
--- 3dMazeGameCapstone/src/Maze.java
+++ 3dMazeGameCapstone/src/Maze.java
@@ -4,5 +4,39 @@
  *
  */
 public class Maze {
-
+	
+	private Block[][][] maze;
+	private int EASY = 12;
+	private int MEDIUM = 24;
+	private int HARD = 30;
+	
+	public Maze (int size) {
+		boolean[][][] tempArr = new boolean[size][size][size];
+		generate (tempArr);
+	}
+	
+	private void generate (boolean [][][] arr) {
+		// Generate Larger Area
+		//    - keep track of volume
+		//    - make sure it is self-contained
+		//    - all contents of this hedged off area is contained in list
+				
+		
+		double randx = (int)(Math.random()*(maze.length - 7) + 3);
+		int randy = (int)(Math.random()*(maze.length - 7) + 3);
+		int randWidth = (int)(Math.random()*2);
+		while (true) {
+			
+		}
+		/* Generate Maze within confined space
+			1. Remove start point from larger list of walls
+			2. 
+				1. Add neighboring walls to wall list.
+					- for a random wall of the wall:
+						- if 
+			2. 
+				- 
+		*/ 
+	}
+	
 }
