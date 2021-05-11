a040f2ed77ae9b2b0b748a6766bc6c29e524cc17
Ameya Gandhi <69989742+ameyagandhi@users.noreply.github.com>
2021-05-10 20:21:42 -0700

    basic class structure done

--- 3dMazeGameCapstone/src/HomeScreen.java
+++ 3dMazeGameCapstone/src/HomeScreen.java
@@ -1,8 +1,8 @@
 import processing.core.PApplet;
 
 /**
- * The first screen a user sees
- * @author ameya
+ * The first screen a user sees has parameters, default is easy mode
+ * @author Ameya
  *
  */
 public class HomeScreen extends PApplet{
@@ -10,6 +10,9 @@ public class HomeScreen extends PApplet{
 	public void draw()
 	{
 		this.text("3D Maze",width/5 ,height/10);
+		this.rect(2*height/3, width/7, width/6, height/10);
+		this.rect(2*height/3, width-width/7-width/6, width/6, height/10);
+		this.rect(3*height/4, width/4, width/2, height/10);
 	
 	}
 }
