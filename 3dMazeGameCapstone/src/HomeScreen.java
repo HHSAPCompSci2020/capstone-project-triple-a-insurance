6b72c121a5077e8ddcaef2cb48b9df76779ad5a2
Ameya Gandhi <69989742+ameyagandhi@users.noreply.github.com>
2021-05-12 15:11:54 -0700

    changing up

--- 3dMazeGameCapstone/src/HomeScreen.java
+++ 3dMazeGameCapstone/src/HomeScreen.java
@@ -7,23 +7,32 @@ import processing.core.PImage;
  *
  */
 public class HomeScreen extends PApplet{
-	private int width, height;
-	PImage backgroundImg;
-	public void setup() {
-		backgroundImg = loadImage("MazeGame.jpg");
+
+	//PImage backgroundImg;
+	public void settings() {
+		size(1000,1000);
+
 	}
-	public void draw()
-	{
-		image(backgroundImg,0,0);
-		this.textSize(24);
-		this.text("3D Maze",width/5 ,height/20);
-		this.rect(width/7, 2*height/3, width/6, height/10);
-		this.textSize(12);
-		this.text("Easy Mode", width/7, 2*height/3, width/7+width/6, 2*height/3+height/10);
-		this.rect(width-width/7-width/6,2*height/3 , width/6, height/10);
-		this.text("Hard Mode", width/7, 2*height/3, width/7+width/6, 2*height/3+height/10);
-		this.rect(width/4, 3*height/4, width/2, height/8);
-		this.text("Play Game", width/4, 3*height/4, width/4+width/2, 3*height/4+height/8);
+	public void setup() {
+		//backgroundImg = loadImage("MazeGame.jpg");
+		
+	}
+	public void draw(){
+		background(100);
+		//image(backgroundImg,0,0);
+		fill(255);
+		rect(width/7, 2*height/3, width/6, height/10);
+		rect(width/4, 3*height/4, width/2, height/8);
+		rect(width-width/7-width/6,2*height/3 , width/6, height/10);
+		textSize(24);
+		text("3D Maze", width/3 , height/4);
+		textSize(12);
+		fill(100);
+		text("Easy Mode", width/7, 2*height/3, width/7+width/6, 2*height/3+height/10);
+		
+		text("Hard Mode", width/7, 2*height/3, width/7+width/6, 2*height/3+height/10);
+		
+		text("Play Game", width/4, 3*height/4, width/4+width/2, 3*height/4+height/8);
 	
 	}
 	
@@ -35,7 +44,15 @@ public class HomeScreen extends PApplet{
 	}
 	
 	
-	public void mousePressed() {
-		
+	public void mouseClicked() {
+		if (this.isChosen(mouseX, mouseY, width/7, 2*height/3, width/6, height/10)){
+			System.out.println("easy mode");
+		}
+		if (this.isChosen(mouseX, mouseY, width-width/7-width/6,2*height/3 , width/6, height/10)) {
+			System.out.println("hard mode");
+		}
+		if (this.isChosen(mouseX, mouseY, width/4, 3*height/4, width/4+width/2, 3*height/4+height/8)) {
+			System.out.println("play game");
+		}
 	}
 }
