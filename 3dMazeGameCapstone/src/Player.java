bb21b6b3e549f17538e41c56e39e7253c8db04d5
Ameya Gandhi <69989742+ameyagandhi@users.noreply.github.com>
2021-05-12 12:40:11 -0700

    started player

--- 3dMazeGameCapstone/src/Player.java
+++ 3dMazeGameCapstone/src/Player.java
@@ -4,5 +4,24 @@
  * 
  */
 public class Player extends Perspective{
+	private float width, height, depth;
+	
+	
+	public float getWidth() {
+		return width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public float getDepth() {
+		return depth;
+	}
+	public void moveTo(float x, float y, float z) {
+		this.getPosition().x = x;
+		this.getPosition().y = y;
+		this.getPosition().z = z;
+	}
 
 }
