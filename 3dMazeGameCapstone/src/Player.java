7586c152b9e217ea846c993e936084303aec9362
Lumijek123 <lakaayush@gmail.com>
2021-05-12 14:56:27 -0700

    formatting

--- 3dMazeGameCapstone/src/Player.java
+++ 3dMazeGameCapstone/src/Player.java
@@ -44,23 +44,23 @@ public class Player extends Camera {
 	 * Checks to see if the player is colliding with any of the Block objects inside
 	 * the specified ArrayList
 	 * 
-	 * @param blocks
-	 *            ArrayList of Block objects to check collision with
+	 * @param blocks ArrayList of Block objects to check collision with
 	 */
 	public void act(ArrayList<Block> blocks) {
 		PVector position = getPosition();
 		PVector velocity = getVelocity();
 		for (Block b : blocks) {
 			// position is in the center of the so you have to
-			//add/substract
+			// add/substract
 			// its (dimension in axis)/2 to get the edges
-			float left = position.x - w / 2;float right = position.x + w / 2;
+			float left = position.x - w / 2;
+			float right = position.x + w / 2;
 			float top = position.y - h / 2;
 			float bottom = position.y + h / 2;
 			float front = position.z + d / 2;
 			float back = position.z - d / 2;
 			// block position is in the center of the block so
-			//you have to add/substract its
+			// you have to add/substract its
 			// dimensions/2 to get the edges
 			float blockSize = b.getSize();
 			float blockHeight = b.getSize();
@@ -72,37 +72,30 @@ public class Player extends Camera {
 			float blockBack = b.getZ() + blockSize / 2;
 			// accordingly
 			if (b.isPointInCube(left, position.y, position.z)) {
-			// move right
-			position.x = blockRight + w / 2;
-			} else if (b.isPointInCube(right, position.y,
-			position.z)) {
-			// move left
-			position.x = blockLeft - w / 2;
+				// move right
+				position.x = blockRight + w / 2;
+			} else if (b.isPointInCube(right, position.y, position.z)) {
+				// move left
+				position.x = blockLeft - w / 2;
 			}
 			if (b.isPointInCube(position.x, top, position.z)) {// move down
-			position.y = blockBottom + h / 2;
-			} else if (b.isPointInCube(position.x, bottom,
-			position.z)) {
-			// move up/grounded
-			position.y = blockTop - h / 2;
-			velocity.y = 0;
-			grounded = true;
+				position.y = blockBottom + h / 2;
+			} else if (b.isPointInCube(position.x, bottom, position.z)) {
+				// move up/grounded
+				position.y = blockTop - h / 2;
+				velocity.y = 0;
+				grounded = true;
 			}
-			if (b.isPointInCube(position.x, position.y, front))
-			{
-			// move back
-			position.z = blockFront - d / 2;
-			} 
-			else if (b.isPointInCube(position.x, position.y,
-			back)) {
-			// move forward
+			if (b.isPointInCube(position.x, position.y, front)) {
+				// move back
+				position.z = blockFront - d / 2;
+			} else if (b.isPointInCube(position.x, position.y, back)) {
+				// move forward
 				position.z = blockBack + d / 2;
 			}
 		}
 		velocity.y += gravity;
 
-
-		
 	}
 
 	public void jump() {
