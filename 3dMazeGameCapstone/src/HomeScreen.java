bb2a0a8d6029458b1e6d541fbb7ba66109bba6d8
Ameya Gandhi <69989742+ameyagandhi@users.noreply.github.com>
2021-05-12 15:43:13 -0700

    UI things

--- 3dMazeGameCapstone/src/HomeScreen.java
+++ 3dMazeGameCapstone/src/HomeScreen.java
@@ -8,30 +8,33 @@ import processing.core.PImage;
  */
 public class HomeScreen extends PApplet{
 
-	//PImage backgroundImg;
+	PImage backgroundImg;
 	public void settings() {
 		size(1000,1000);
 
 	}
 	public void setup() {
-		//backgroundImg = loadImage("MazeGame.jpg");
+		backgroundImg = loadImage("MazeGame.jpg");
 		
 	}
 	public void draw(){
 		background(100);
-		//image(backgroundImg,0,0);
-		fill(255);
+		image(backgroundImg,0,0,width,height);
+		fill(200);
 		rect(width/4, 3*height/4, width/2, height/8);
-		rect(width/7, height/2, width/6, height/10);
-		rect(width-width/7-width/6,height/2 , width/6, height/10);
-		textSize(24);
-		text("3D Maze", width/3 , height/4);
+		rect(width/6, height/2, width/7, height/10);
+		rect(width-width/7-width/6,height/2 , width/7, height/10);
+		rect(width/3+20,height/6 , width/3, height/10);
+		textSize(50);
+		fill(0);
+		text("3D Maze", 5*width/12 , height/4);
 		textSize(16);
+		textAlign(LEFT);
 		fill(100);
-		text("Easy Mode", width/7, height/2, width/7+width/6, 2*height/3+height/10);
+		text("Easy Mode", width/6, height/2, width/6+width/7, 2*height/3+height/10);
 		
-		text("Hard Mode", width-width/7-width/6, height/2, width-width/7, 2*height/3+height/10);
-		
+		text("Hard Mode", width-width/7-width/6, height/2, width-width/6, 2*height/3+height/10);
+		textSize(40);
 		text("Play Game", width/4, 3*height/4, width/4+width/2, 3*height/4+height/8);
 	
 	}
@@ -48,7 +51,7 @@ public class HomeScreen extends PApplet{
 		if (this.isChosen(mouseX, mouseY, width/7, height/2, width/7+width/6, 2*height/3+height/10)){
 			System.out.println("easy mode");
 		}
-		if (this.isChosen(mouseX, mouseY,width-width/7-width/6, height/2, width-width/7, 2*height/3+height/10)) {
+		if (this.isChosen(mouseX, mouseY,width-width/7-width/6, height/2, width-width/6, 2*height/3+height/10)) {
 			System.out.println("hard mode");
 		}
 		if (this.isChosen(mouseX, mouseY, width/4, 3*height/4, width/4+width/2, 3*height/4+height/8)) {
