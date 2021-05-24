a3d3481c06d4731c08ed29346573f9ef5d57690d
Ameya Gandhi <69989742+ameyagandhi@users.noreply.github.com>
2021-05-23 19:24:34 -0700

    remove prints

--- 3dMazeGameCapstone/src/Player.java
+++ 3dMazeGameCapstone/src/Player.java
@@ -21,7 +21,7 @@ public class Player extends Camera {
 		// speed is at .12f max
 		this(5, 3, 1, .04f, .5f, .5f, .75f, PConstants.PI / 3f, 80f);
 		this.start = start;
-		System.out.println("" + start.x + " "+ start.y + " "  +start.z);
+		
 	}
 
 	/**
