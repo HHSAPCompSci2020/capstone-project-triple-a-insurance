973e3dd1a5ca5355e1c3a8064e5e682a3de1a6df
Lumijek123 <lakaayush@gmail.com>
2021-05-23 23:31:42 -0700

    Timer javadoc

--- 3dMazeGameCapstone/src/LevelTimer.java
+++ 3dMazeGameCapstone/src/LevelTimer.java
@@ -35,25 +35,43 @@ public class LevelTimer {
 		this.timeLeft = timeLeft;	
 	}
 	
+	/**
+	 * Starts the timer
+	 */
 	public void startTimer() {
 
 		currentTimeLeft.start();		
 
 	}
+	/**
+	 * Restarts timer with the new time
+	 * @param timeLeft the new time
+	 */
 	public void restartTimer(int timeLeft) {
 		this.timeLeft = timeLeft;
 	}
 	
+	/**
+	 * Stops the timer
+	 */
 	public void stopTimer() {
 		
 		currentTimeLeft.stop();
 		
 	}
 	
+	/**
+	 * Returns the time left in the timer
+	 * @return the time left in the timer
+	 */
 	public int getTimeLeft() {
 		return timeLeft;
 	}
 	
+	/**
+	 * Returns the amount of zeros to make sure the time is displayed in 3 digits at all times
+	 * @return the amount of zeros to make the length 3
+	 */
 	public String timeStartingZeros() {
 		int tl = 3 - Integer.toString(timeLeft).length();
 		String displayTime = "";
@@ -63,6 +81,9 @@ public class LevelTimer {
 		return displayTime;
 	}
 	
+	/**
+	 * Decreases the time left on the timer by one
+	 */
 	public void decreaseTime() {
 		timeLeft--;
 	}
